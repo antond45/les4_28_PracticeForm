65b93471f175520f676cb45d0eb229245408a484
Darin Anton <adarin@t1-consulting.ru>
2024-08-03 00:57:47 +0300

    add Attach

--- src/test/java/NewPracticeFormTest.java
+++ src/test/java/NewPracticeFormTest.java
@@ -1,4 +1,4 @@
-import com.github.javafaker.Faker;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -34,9 +34,5 @@ public class TestBase {
     @AfterEach
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-        Selenide.closeWebDriver();
     }
 }
